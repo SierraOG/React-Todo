ea45a1452229456128e15269c730bc38aed0e2a0
SierraOG <sierrao@utexas.edu>
2019-08-05 14:41:50 -0500

    todos show up on page

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoForm = () => {
+    return (
+        <>
+        <form >
+            <input />
+            <button type='submit' >Add a todo</button>
+        </form>
+        <button>Clear Completed</button>
+        </>
+    )
+}
